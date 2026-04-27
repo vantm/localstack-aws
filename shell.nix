243b3eb152f2596c46b80796267491e15fceefbe
Van Tran <tmvan.1903@gmail.com>
2026-04-27 17:53:31 +0700

    feat: init repo

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{
+  pkgs ? import <nixpkgs> {
+    config.allowUnfreePredicate =
+      pkg:
+      builtins.elem (pkgs.lib.getName pkg) [
+        "terraform"
+      ];
+  },
+  nur ?
+    import
+      (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/9e5cd1c163821839337ba993f895fa8109f93dec.tar.gz")
+      {
+        inherit pkgs;
+      },
+}:
+pkgs.mkShellNoCC {
+  packages =
+    (with pkgs; [
+      localstack
+      terraform
+      python3
+      awscli
+      terraform-local
+    ])
+    ++ (with nur; [
+      repos.anthonyroussel.awscli-local
+    ]);
+
+  shellHook = ''
+    localstack start -d
+  '';
+}
