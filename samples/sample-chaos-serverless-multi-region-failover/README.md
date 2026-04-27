eb456f48d7a78c1c366d659f347b2510b28e765b
Van Tran <tmvan.1903@gmail.com>
2026-04-27 18:33:55 +0700

    feat: add samples

---
+++ samples/sample-chaos-serverless-multi-region-failover/README.md
@@ -0,0 +1,15 @@
+# Nix-specific commands
+
+## Installation
+
+Navigate to the source code directory:
+
+```bash
+cd source/
+```
+
+Then, build the project using Nix:
+
+```bash
+make SHELL=/bin/sh install
+```
---
+++ samples/sample-chaos-serverless-multi-region-failover/shell.nix
@@ -0,0 +1,34 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  venvDir = "./.venv";
+  pythonPkgs = pkgs.python3Packages;
+in
+pkgs.mkShell {
+  packages = [
+    pythonPkgs.python
+  ]
+  ++ (with pkgs; [
+    openjdk17
+    maven
+  ]);
+
+  REPO_URL = "https://github.com/localstack-samples/sample-chaos-serverless-multi-region-failover.git";
+
+  shellHook = ''
+    if [ ! -d "./source" ] ; then
+      ${pkgs.git}/bin/git clone $REPO_URL ./source
+    fi
+
+    if [ -d "$venvDir" ] ; then
+      echo "Skipping virtual environment creation, already exists at ${venvDir}";
+    else
+      echo "Creating virtual environment at ${venvDir}...";
+      ${pythonPkgs.python.interpreter} -m venv "${venvDir}"
+      echo "Virtual environment created at ${venvDir}!";
+    fi
+
+    source "${venvDir}/bin/activate"
+  '';
+}
