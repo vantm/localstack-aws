c2c43ca9108a9144eaf5d214833a969375376854
Van Tran <tmvan.1903@gmail.com>
2026-05-02 12:14:28 +0700

    feat: cognito avoid token re-generated everytime we apply new changes.

--- samples/convert-and-watermark-documents/cognito.tf
+++ samples/convert-and-watermark-documents/cognito.tf
@@ -10,13 +10,17 @@ resource "aws_cognito_user_pool" "documents_pool" {
   }
 
   auto_verified_attributes = ["email"]
+
+  software_token_mfa_configuration {
+    enabled = false
+  }
 }
 
 # Cognito User Pool Client
 resource "aws_cognito_user_pool_client" "documents_client" {
   name            = "documents-api-client"
   user_pool_id    = aws_cognito_user_pool.documents_pool.id
-  generate_secret = true
+  generate_secret = false
   token_validity_units {
     access_token  = "minutes"
     refresh_token = "days"
