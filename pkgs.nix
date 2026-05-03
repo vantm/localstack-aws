7b868194d7b9d7e89755d6b57e2a9f2c54892681
Van Tran <tmvan.1903@gmail.com>
2026-05-03 11:49:04 +0700

    chore: pin nixpkgs version

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = import <nixpkgs> { };
-  nixpkgs-unstable = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
+  nixpkgs-unstable = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/c6d65881c5624c9cae5ea6cedef24699b0c0a4c0.tar.gz";
 in
 with nixpkgs;
 import nixpkgs-unstable {
