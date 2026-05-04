e1c1115cb06db18db0e83269ba480597a6cadaeb
Van Tran <tmvan.1903@gmail.com>
2026-05-04 23:26:31 +0700

    feat: add local, dev envs

---
+++ samples/convert-and-watermark-documents/terraform/envs/local/main.tf
@@ -0,0 +1,24 @@
+module "root" {
+  source = "../../"
+
+  environment                 = var.environment
+  project_name                = var.project_name
+  aws_region                  = var.aws_region
+  dynamodb_table_name         = var.dynamodb_table_name
+  dynamodb_access_policy_name = var.dynamodb_access_policy_name
+  user_pool_name              = var.user_pool_name
+  user_pool_client_name       = var.user_pool_client_name
+  watermark_text              = var.watermark_text
+  output_format               = var.output_format
+  api_name                    = var.api_name
+  api_description             = var.api_description
+  authorizer_credentials_arn  = var.authorizer_credentials_arn
+  monitoring_dashboard_name   = var.monitoring_dashboard_name
+  logs_retention_in_days      = var.logs_retention_in_days
+  waf_name                    = var.waf_name
+  waf_description             = var.waf_description
+  waf_rate_limit              = var.waf_rate_limit
+  convert_result_bucket       = var.convert_result_bucket
+  watermark_result_bucket     = var.watermark_result_bucket
+  queue_name                  = var.queue_name
+}
---
+++ samples/convert-and-watermark-documents/terraform/envs/local/variables.tf
@@ -0,0 +1,79 @@
+variable "environment" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "dynamodb_table_name" {
+  type = string
+}
+
+variable "dynamodb_access_policy_name" {
+  type = string
+}
+
+variable "user_pool_name" {
+  type = string
+}
+
+variable "user_pool_client_name" {
+  type = string
+}
+
+variable "watermark_text" {
+  type = string
+}
+
+variable "output_format" {
+  type = string
+}
+
+variable "api_name" {
+  type = string
+}
+
+variable "api_description" {
+  type = string
+}
+
+variable "authorizer_credentials_arn" {
+  type = string
+}
+
+variable "monitoring_dashboard_name" {
+  type = string
+}
+
+variable "logs_retention_in_days" {
+  type = number
+}
+
+variable "waf_name" {
+  type = string
+}
+
+variable "waf_description" {
+  type = string
+}
+
+variable "waf_rate_limit" {
+  type = number
+}
+
+variable "convert_result_bucket" {
+  type = string
+}
+
+variable "watermark_result_bucket" {
+  type = string
+}
+
+variable "queue_name" {
+  type = string
+}
